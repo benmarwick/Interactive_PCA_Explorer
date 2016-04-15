4df400a9a1663ccd992065b852549eaca0b01134
Ben Marwick <benmarwick@gmail.com>
2016-04-16 05:35:51 +1000

    inital

---
+++ server.R
@@ -0,0 +1,275 @@
+# global items 
+library("ggplot2")
+library("DT")
+library("GGally")
+library("psych")
+library("Hmisc")
+library("MASS")
+
+server <- function(input, output) {
+  
+  # By default, the file size limit is 5MB. It can be changed by
+  # setting this option. Here we'll raise limit to 9MB.
+  options(shiny.maxRequestSize = 9*1024^2)
+  
+  # read in the CSV
+  the_data_fn <- reactive({
+    inFile <- input$file1
+    if (is.null(inFile)) return(NULL)
+    the_data <-   read.csv(inFile$datapath, header = (input$header == "Yes"),
+                               sep = input$sep, quote = input$quote)
+    return(the_data)
+  })
+  
+  # display a table of the CSV contents
+  output$contents <-  DT::renderDataTable({
+    #
+    the_data_fn()
+  })
+  
+  # display a summary of the CSV contents
+  output$summary <-  renderTable({
+    the_data <- the_data_fn()
+    psych::describe(the_data)
+  })
+  
+  # Check boxes to choose columns
+  output$choose_columns_biplot <- renderUI({
+    
+    the_data <- the_data_fn()
+    
+    colnames <- names(the_data)
+    
+    # Create the checkboxes and select them all by default
+    checkboxGroupInput("columns_biplot", "Choose up to five columns to display on the scatterplot matrix", 
+                       choices  = colnames,
+                       selected = colnames)
+  })
+  
+  # corr plot
+  output$corr_plot <- renderPlot({
+    the_data <- the_data_fn()
+    # Keep the selected columns
+    columns_biplot <-    input$columns_biplot
+    the_data_subset_biplot <- the_data[, columns_biplot, drop = FALSE]
+    ggpairs(the_data_subset_biplot)
+     })
+  
+  # corr tables
+  output$corr_tables <- renderTable({
+    the_data <- the_data_fn()
+    # we only want to show numeric cols
+    the_data_num <- the_data[,sapply(the_data,is.numeric)]
+      res <- Hmisc::rcorr(as.matrix(the_data_num))
+      cormat <- res$r
+      pmat <- res$P
+      ut <- upper.tri(cormat)
+     df <- data.frame(
+        row = rownames(cormat)[row(cormat)[ut]],
+        column = rownames(cormat)[col(cormat)[ut]],
+        cor  = (cormat)[ut],
+        p = pmat[ut]
+      )
+     with(df, df[order(-cor), ])
+    
+  })
+  
+output$bartlett <- renderPrint({
+  the_data <- the_data_fn()
+  the_data_num <- the_data[,sapply(the_data,is.numeric)]
+  
+  cortest.bartlett(cor(the_data_num), n = nrow(the_data_num))
+})  
+
+output$kmo <- renderPrint({
+  the_data <- the_data_fn()
+  the_data_num <- the_data[,sapply(the_data,is.numeric)]
+ # R <- cor(the_data_num)
+ # KMO(R)
+  
+  # http://www.opensubscriber.com/message/r-help@stat.math.ethz.ch/7315408.html
+  # KMO Kaiser-Meyer-Olkin Measure of Sampling Adequacy 
+  kmo = function( data ){ 
+    
+    library(MASS) 
+    X <- cor(as.matrix(data)) 
+    iX <- ginv(X) 
+    S2 <- diag(diag((iX^-1))) 
+    AIS <- S2%*%iX%*%S2                      # anti-image covariance matrix 
+    IS <- X+AIS-2*S2                         # image covariance matrix 
+    Dai <- sqrt(diag(diag(AIS))) 
+    IR <- ginv(Dai)%*%IS%*%ginv(Dai)         # image correlation matrix 
+    AIR <- ginv(Dai)%*%AIS%*%ginv(Dai)       # anti-image correlation matrix 
+    a <- apply((AIR - diag(diag(AIR)))^2, 2, sum) 
+    AA <- sum(a) 
+    b <- apply((X - diag(nrow(X)))^2, 2, sum) 
+    BB <- sum(b) 
+    MSA <- b/(b+a)                        # indiv. measures of sampling adequacy 
+    
+    AIR <- AIR-diag(nrow(AIR))+diag(MSA)  # Examine the anti-image of the 
+    # correlation matrix. That is the 
+    # negative of the partial correlations, 
+    # partialling out all other variables. 
+    
+    kmo <- BB/(AA+BB)                     # overall KMO statistic 
+    
+    # Reporting the conclusion 
+    if (kmo >= 0.00 && kmo < 0.50){ 
+      test <- 'The KMO test yields a degree of common variance 
+      unacceptable for FA.' 
+    } else if (kmo >= 0.50 && kmo < 0.60){ 
+      test <- 'The KMO test yields a degree of common variance miserable.' 
+    } else if (kmo >= 0.60 && kmo < 0.70){ 
+      test <- 'The KMO test yields a degree of common variance mediocre.' 
+    } else if (kmo >= 0.70 && kmo < 0.80){ 
+      test <- 'The KMO test yields a degree of common variance middling.' 
+    } else if (kmo >= 0.80 && kmo < 0.90){ 
+      test <- 'The KMO test yields a degree of common variance meritorious.' 
+    } else { 
+      test <- 'The KMO test yields a degree of common variance marvelous.' 
+    } 
+    
+    ans <- list(  overall = kmo, 
+                  report = test, 
+                  individual = MSA, 
+                  AIS = AIS, 
+                  AIR = AIR ) 
+    return(ans) 
+    
+  }    # end of kmo() 
+  kmo(the_data_num)
+  
+}) 
+  
+ 
+  
+  # Check boxes to choose columns
+  output$choose_columns_pca <- renderUI({
+    
+    the_data <- the_data_fn()
+    
+    # Get the data set with the appropriate name
+    
+    # we only want to show numeric cols
+    the_data_num <- the_data[,sapply(the_data,is.numeric)]
+    
+    colnames <- names(the_data_num)
+    
+    # Create the checkboxes and select them all by default
+    checkboxGroupInput("columns", "Choose columns", 
+                       choices  = colnames,
+                       selected = colnames)
+  })
+  
+  # choose a grouping variable
+  output$the_grouping_variable <- renderUI({
+    the_data <- the_data_fn()
+
+              
+  # only show integer or character cols here
+    the_data_group_cols <- the_data[,!sapply(the_data,is.numeric), drop = FALSE]
+    # drop down selection
+    selectInput(inputId = "the_grouping_variable", 
+                label = "Grouping variable:",
+                choices=names(the_data_group_cols))
+
+  })
+  
+  
+pca_objects <- reactive({
+  # Keep the selected columns
+  columns <-    input$columns
+  the_data <- the_data_fn()
+  the_data_subset <- the_data[, columns, drop = FALSE]
+  
+  # from http://rpubs.com/sinhrks/plot_pca
+  pca_output <- prcomp(the_data_subset, 
+                       center = (input$center == 'Yes'), 
+                       scale. = (input$scale. == 'Yes'))
+  # data.frame of PCs
+  pcs_df <- cbind(the_data, pca_output$x)
+  
+  return(list(the_data = the_data, 
+       the_data_subset = the_data_subset,
+       pca_output = pca_output, 
+       pcs_df = pcs_df))
+  
+})
+
+output$the_pcs_to_plot_x <- renderUI({
+  pca_output <- pca_objects()$pca_output$x
+  
+  # drop down selection
+  selectInput(inputId = "the_pcs_to_plot_x", 
+              label = "X axis:",
+              choices= colnames(pca_output), 
+              selected = 'PC1')
+})
+
+output$the_pcs_to_plot_y <- renderUI({
+  pca_output <- pca_objects()$pca_output$x
+  
+  # drop down selection
+  selectInput(inputId = "the_pcs_to_plot_y", 
+              label = "Y axis:",
+              choices= colnames(pca_output), 
+              selected = 'PC2')
+})
+  
+
+
+  output$plot2 <- renderPlot({
+    pca_output <- pca_objects()$pca_output
+    eig = (pca_output$sdev)^2
+    variance <- eig*100/sum(eig)
+    cumvar <- paste(round(cumsum(variance),1), "%")
+    eig_df <- data.frame(eig = eig,
+                         PCs = colnames(pca_output$x),
+                         cumvar =  cumvar)
+    ggplot(eig_df, aes(PCs, eig)) +
+      geom_bar(stat = "identity", fill = "white", colour = "black") +
+      geom_text(label = cumvar, size = 5,
+                vjust=-0.4) +
+      theme_bw(base_size = 14) +
+      xlab("PC") +
+      ylab("Variances")
+  })
+  
+  
+  # PC plot
+  output$plot1 <- renderPlot({
+    pcs_df <- pca_objects()$pcs_df
+    pca_output <-  pca_objects()$pca_output
+    
+    var_expl_x <- round(100 * pca_output$sdev[as.numeric(gsub("[^0-9]", "", input$the_pcs_to_plot_x))]^2/sum(pca_output$sdev^2), 1)
+    var_expl_y <- round(100 * pca_output$sdev[as.numeric(gsub("[^0-9]", "", input$the_pcs_to_plot_y))]^2/sum(pca_output$sdev^2), 1)
+
+
+    pc_plot  <- ggplot(pcs_df, aes_string(input$the_pcs_to_plot_x, 
+                                          input$the_pcs_to_plot_y, 
+                                          fill = input$the_grouping_variable, 
+                                          colour = input$the_grouping_variable)) +
+      stat_ellipse(geom = "polygon", alpha = 0.1) +
+      geom_text(aes(label = rownames(pca_objects()$the_data)), size = 5) +
+      theme_bw(base_size = 14) +
+      coord_equal() +
+      xlab(paste0(input$the_pcs_to_plot_x, " (", var_expl_x, "% explained variance)")) +
+      ylab(paste0(input$the_pcs_to_plot_y, " (", var_expl_y, "% explained variance)")) 
+    # the plot
+    pc_plot
+    
+    
+  })
+  
+  output$info <- renderPrint({
+    # the brushing function
+    brushedPoints(pca_objects()$pcs_df, input$plot_brush)
+  })
+  
+  output$pca_details <- renderPrint({
+    # 
+    print(pca_objects()$pca_output$rotation)
+    summary(pca_objects()$pca_output)
+    
+  })
+}
---
+++ ui.R
@@ -0,0 +1,121 @@
+ui <- bootstrapPage(
+  mainPanel(
+        
+        tabsetPanel(
+          
+          tabPanel("Data input", 
+                   
+                   fileInput('file1', 'Choose a CSV file to upload',
+                             accept = c(
+                               'text/csv',
+                               'text/comma-separated-values',
+                               'text/tab-separated-values',
+                               'text/plain',
+                               '.csv',
+                               '.tsv'
+                             )),
+                   
+
+                   radioButtons(inputId = 'header',  
+                                label = 'Header',
+                                choices = c('Columns have headers'='Yes',
+                                            'Columns do not have headers'='No'), 
+                                selected = 'Yes'),
+          
+                   radioButtons('sep', 'Separator',
+                                c(Comma=',',
+                                  Semicolon=';',
+                                  Tab='\t'),
+                                ','),
+          
+                   radioButtons('quote', 'Quote',
+                                c(None='',
+                                  'Double Quote'='"',
+                                  'Single Quote'="'"),
+                                '"'),
+                   
+                   tags$hr(),
+                   
+                   p("Here is a summary of the data"),
+                   tableOutput('summary'),
+                   tags$hr(),
+                   p("Here is the raw data from the CSV file"),
+                   DT::dataTableOutput('contents')
+                   
+                   
+                   
+                
+                   
+                   ), # end file  tab
+          
+          tabPanel("Correlation Plots",
+                   uiOutput("choose_columns_biplot"),
+                   tags$hr(),
+                   p("This plot may take a few moments to appear when analysing large datasets. You may want to exclude highly correlated variables from the PCA."),
+                   
+                   plotOutput("corr_plot"),
+                   tags$hr(),
+                   p("Summary of correlations"),
+                   tableOutput("corr_tables")
+          ), # end  tab
+          
+          tabPanel("Diagnostics",
+                   
+                   p("Among SPSS users, these tests are considered to provide some guidelines on the suitability of the data for a principal components analysis. However, they may be safely ignored in favour of common sense."),
+                   tags$hr(),
+                   p("Here is the output of Bartlett’s sphericity test. Bartlett's test of sphericity tests whether the data comes from multivariate normal distribution with zero covariances. If p > 0.05 then PCA may not be very informative"),
+                   verbatimTextOutput("bartlett"),
+                   tags$hr(),
+                   p("Here is the output of the Kaiser-Meyer-Olkin (KMO) index test. The overall measure varies between 0 and 1, and values closer to 1 are better. A value of 0.6 is a suggested minimum. "),
+                   verbatimTextOutput("kmo")
+                   
+                   
+                   
+          ), # end  tab
+          
+          tabPanel("Compute PCA",
+                   
+                   p("Choose the columns of your data to include in the PCA. Only columns containing numeric data are shown here because PCA doesn't work with non-numeric data."),
+                   uiOutput("choose_columns_pca"),
+                   tags$hr(),
+                   p("Select options for the PCA computation (we are using the prcomp function here)"),
+                   radioButtons(inputId = 'center',  
+                                label = 'Center',
+                                choices = c('Shift variables to be zero centered'='Yes',
+                                            'Do not shift variables'='No'), 
+                                selected = 'Yes'),
+                   
+                   radioButtons('scale.', 'Scale',
+                                choices = c('Scale variables to have unit variance'='Yes',
+                                            'Do not scale variables'='No'), 
+                                selected = 'Yes')
+                   
+          ), # end  tab
+          
+
+      
+          tabPanel("PC Plots",
+                   p("The scree plot shows the variences of each PC, and the varience explained by each PC (in %) "),
+                   plotOutput("plot2"),
+                   tags$hr(),
+                   p("Select the grouping variable (only columns of character and integer type are shown here)"),
+                   uiOutput("the_grouping_variable"),
+                   tags$hr(),
+                   p("Select the PCs to plot"),
+                   uiOutput("the_pcs_to_plot_x"),
+                   uiOutput("the_pcs_to_plot_y"),
+                   tags$hr(),
+                   p("Click and drag on the plot to select points, and inspect the table of selected points below"),
+                   plotOutput("plot1",  brush = "plot_brush", height = "400px"),
+                   tags$hr(),
+                   p("Details of the selected points"),
+                   verbatimTextOutput("info")
+          ), # end  tab 
+          
+          tabPanel("PCA output",
+                   verbatimTextOutput("pca_details")
+                   
+          ) # end  tab 
+          
+          
+          ))) 
