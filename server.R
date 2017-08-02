d9db32eae805c5635e427213832271f113d9bd27
Jason Greenbaum <jgbaum@dhcp-10-89.liai.org>
2017-08-02 08:32:47 -0700

    removed dependency installation; removing R project file

--- server.R
+++ server.R
@@ -13,7 +13,7 @@ list.of.packages <- c("ggplot2",
                       "tabplot")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+#if(length(new.packages)) install.packages(new.packages)
 
 # load all these
 lapply(list.of.packages, require, character.only = TRUE)
